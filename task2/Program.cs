f1e218ce080a5533c67853aa0226d2565fe59e8c
EugeneNikiforov <eugene_vn@mail.ru>
2023-07-06 21:15:54 +0500

    add task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+int index = 2;
+while (index <= number)
+{
+    Console.WriteLine(index);
+    index += 2;
+}
